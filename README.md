2829d1df781dbb6dea6d82da07419c99993e63e3
alex <alex@activewidgets.com>
2019-05-07 17:27:43 +0200

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+
+ActiveWidgets grid plugins - see http://www.activewidgets.com
+
+Copyright (C) 2003-2018 ActiveWidgets SARL. All Rights Reserved. 
---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+let main = {plugins: 'index.js'};
+
+let globals = {};
+
+let umd = {dir: 'dist', entryFileNames: 'ax-[name].js', format: 'umd', sourcemap: true, name: 'AX.plugins', extend: true, globals},
+    esm = {dir: 'dist', entryFileNames: 'ax-[name].esm.js', format: 'esm', sourcemap: true};
+
+let external = Object.keys(globals);
+
+let plugins = [
+    resolve(),
+    babel({
+        babelrc: false,
+        exclude: 'node_modules/**',
+        presets: [["@babel/env", {modules: false}]]
+    })
+];
+
+export default [
+    {input: main, output: umd, external, plugins},
+    {input: main, output: esm, external, plugins}
+]
