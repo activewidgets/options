b58d3eade95a0d0b3190bba44c35c0843ce42717
alex <alex@activewidgets.com>
2019-12-16 14:32:08 +0100

    build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import {terser} from 'rollup-plugin-terser';
+import fs from 'fs';
+import path from 'path';
 
-let main = {options: 'index.js'};
-
-let umd = {dir: 'dist', entryFileNames: 'ax-[name].umd.js', format: 'umd', sourcemap: true, name: 'ActiveWidgets.Options', extend: true},
-    esm = {dir: 'dist', entryFileNames: 'ax-[name].esm.js', format: 'esm', sourcemap: true};
 
 let plugins = [
     resolve(),
@@ -13,10 +12,49 @@ let plugins = [
         babelrc: false,
         exclude: 'node_modules/**',
         presets: [["@babel/env", {modules: false}]]
-    })
+    }),
+    terser()
 ];
 
-export default [
-    {input: main, output: umd, plugins},
-    {input: main, output: esm, plugins}
-]
+
+function read(filename){
+    return String(fs.readFileSync(filename, {encoding:'utf8'}));
+}
+
+
+function expand({name, input}){
+
+    let dir = path.dirname(input),
+        pkg = JSON.parse(read(path.join(dir, 'package.json'))),
+        main = path.join(dir, pkg.main),
+        module = path.join(dir, pkg.module),
+        browser = path.join('dist', path.basename(pkg.name) + '.js'),
+        sourcemap = true,
+        extend = true;
+
+    return {
+        input,
+        output: [
+            {file: main, format: 'cjs', sourcemap},
+            {file: module, format: 'esm', sourcemap},
+            {file: browser, format: 'umd', sourcemap, name, extend}
+        ],
+        plugins
+    };
+}
+
+
+function config(name, input){
+
+    let re = /default as (\w+).+'(.+)'/;
+
+    return read(input)
+        .split('\n')
+        .map(s => s.match(re) ? {name: name + '.' + RegExp.$1, input: RegExp.$2} : null)
+        .filter(x => !!x)
+        .concat({name, input})
+        .map(expand);
+}
+
+
+export default config('ActiveWidgets.Options', 'index.js');
