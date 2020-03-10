8923f0ad4c3b63c523c2c9e461036ee7f87a5aa5
alex <alex@activewidgets.com>
2020-03-10 16:44:28 +0100

    sorting

--- sort/index.js
+++ sort/index.js
@@ -5,36 +5,89 @@
  */
 
 
-function plugin({props, update, on, emit, watch}){
+function plugin({props, update, on, emit, watch, cls}){
 
-    let {sorting = {}} = props();
+    let {sorting = {items: []}} = props(),
+        columns = {},
+        sorted = {},
+        composite = () => 0;
+
+
+    function makeSortFn(items){
+
+        let {key, direction} = items[0],
+            col = columns[key],
+            dir = direction === 'desc' ? -1 : 1,
+            compare = col.compare;
+
+        sorted[key] = direction;
+
+        return function(data1, data2){
+            return dir * compare(data1, data2);
+        };
+    }
+
 
     watch('sorting', function(value){
+
+        sorted = {};
         sorting = value;
+        composite = makeSortFn(value.items);
         emit('reload');
     });
 
+
+    function compare(v1, v2){
+        return v1 > v2 ? 1 : (v2 > v1 ? -1 : 0);
+    }
+
+
+    on('sortingChange', function(value){
+        emit('sort', value);
+    });
+
+
     on('column', function(col){
-        col.header.actions.default = () => {
-            update({sorting: {by: col.key}});
+
+        col.compare = function(data1, data2){
+            return compare(col.value(data1), col.value(data2));
         };
 
-        if (sorting.by === col.key){
-            col.header.style = {color: 'red'};
+        if (sorted[col.key]){
+            col.direction = sorted[col.key];
+            col.header[cls] = 'ax-sort-' + col.direction;
         }
+
+        columns[col.key] = col;
     });
+
 
     on('data', function(context){
 
         let {target, data} = context;
 
-        if (target === 'rows' && sorting.by && data && Array.isArray(data)){
-            //...
-            context.data = data.reverse();
+        if (target === 'rows' && sorting.items.length && data && Array.isArray(data)){
+            context.data = data.sort(composite);
+        }
+    });
+
+
+    on('mouse', null, function({done, row, column}){
+        if (!done && row.type == 'headers'){
+            update({sorting: sort(column)});
         }
     });
 }
 
-export default function(){
-    return {plugin};
+
+export default function sort(col = {}){
+
+    let {key, field, direction} = col;
+
+    return {
+        items: [{key, field, direction: (direction === 'asc' ? 'desc' : 'asc')}],
+        plugin
+    };
 }
+
+
